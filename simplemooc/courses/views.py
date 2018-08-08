c46eb97781f4f2ebf7c073b74eab4281457636d1
Andre Luiz Ximenes <andreluizximenes@gmail.com>
2018-08-07 21:28:46 -0300

    ajustando o css para criacao do template da pagina de cursos

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
 def index(request):
     template_name='courses/index.html'
-    return render(request, template_name)
+    courses = Course.objects.all()
+    context = {
+        'courses': courses
+    }
+    return render(request, template_name, context)
